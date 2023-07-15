455d7993a35185bd1c899e705ff30e6001cf7a7e
Damian Panes <70489858+dampokan@users.noreply.github.com>
2023-07-15 12:52:13 -0400

    Añadiendo inicio de sesión y registro con encriptación

--- main.py
+++ main.py
@@ -1,28 +1,24 @@
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from model.usuarios import Usuarios
-from handler.usuarios import handleUsers 
+from handler.usuarios import handleUsers
+
 
 app = FastAPI()
 app.add_middleware(
-    CORSMiddleware, 
-    allow_origins = ["*"],
-    allow_credentials = True,
-    allow_methods = ["*"],
-    allow_headers= ["*"],
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
+
 
 # Raiz
 @app.get("/api/")
-def buscarUsuarios():
-    return {"Hola":" Molto Deli"}
+def raiz():
+    return {"Hola": " Molto Deli"}
 
-# Usuarios
-@app.get("/api/usuarios")
-def buscarUsuarios():
-    usuarios = handleUsers.buscarUsuarios()
-    return usuarios
 
-@app.get("/api/usuario/{id}")
-def buscarUsuario(id):
-    usuarioBuscado = handleUsers.buscarUsuario()
+test = Usuarios.iniciarSesion("juan@gmail.com", "jfihfuhsdf")
+print(test)
--- model/usuarios.py
+++ model/usuarios.py
@@ -1,26 +1,50 @@
+import os
 from connection.connection import Conecction
+from dotenv import load_dotenv
+from cryptography.fernet import Fernet
+import base64
+
 connection = Conecction()
 
+load_dotenv()
+
+
+def cargar_clave():
+    clave = os.getenv("CLAVE_ENCRIPTACION").encode()
+    return clave
+
+
+def encriptar(contrasena, clave):
+    crypter = Fernet(base64.urlsafe_b64encode(clave.ljust(32, b"\0")))
+    ce = crypter.encrypt((contrasena.encode()))
+    return ce
+
+
+def desencriptar(contrasena, clave):
+    crypter = Fernet(base64.urlsafe_b64encode(clave.ljust(32, b"\0")))
+    cd = crypter.decrypt(contrasena.encode())
+    return cd
+
+
 class Usuarios:
-    def buscarUsuarios():
-        query = "SELECT * FROM usuarios"
-        connection.conectar()
-        resultado = connection.consultaDB(query, 2)
-        connection.desconectar()
-        return resultado
-        
-    def buscarUsuario(id):
-        query = "SELECT * FROM usuarios WHERE userid = %s"
-        parametros = id,
-        connection.conectar()
-        resultado = connection.consultaDB(query, 2, parametros)
-        connection.desconectar()
-        return resultado
-    
     def registrarUsuario(nombre, correo, contrasena):
-        query = "INSERT INTO usuarios (nombre, correo, contrasena) VALUES (%s, %s, %s)" 
-        parametros = nombre, correo, contrasena
+        contrasenaEncriptada = encriptar(contrasena, cargar_clave())
+        query = "INSERT INTO usuarios (nombre, correo, contraseña) VALUES (%s, %s, %s)"
+        parametros = nombre, correo, contrasenaEncriptada
         connection.conectar()
         resultado = connection.consultaDB(query, 1, parametros)
         connection.desconectar()
         return resultado
+
+    def iniciarSesion(correo, contrasena):
+        query = "SELECT * FROM usuarios WHERE correo = %s"
+        parametros = (correo,)
+        connection.conectar()
+        contrasenaValida = False
+        resultado = connection.consultaDB(query, 2, parametros)
+        contrasenaDesencriptada = desencriptar(resultado[0][3], cargar_clave())
+        if str(contrasenaDesencriptada, "utf8") == contrasena:
+            contrasenaValida = True
+        else:
+            contrasenaValida = False
+        return contrasenaValida
